34acb3d6f3fe369ed9d6d96e1fc949bdcd855739
Mati <matipacheco@gmail.com>
2020-02-08 14:12:34 -0300

    for some reason graphql explodes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,27 @@
 import express from 'express';
+import graphqlHTTP from "express-graphql";
+import schema from "../../src/graphql/schema";
+
 let router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res) => res.send('Hello World!'));
+router.post('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: {
+    hello: () => {
+      return 'Hello world!';
+    },
+  },
+  graphiql: false,
+}));
+
+router.get('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: {
+    hello: () => {
+      return 'Hello world!';
+    },
+  },
+  graphiql: true,
+}));
 
 export default router;
